274c5344c38210e6ca8c33da0ec77353bf9c9aff
Omar Mustafa <77558263+omarmustafa130@users.noreply.github.com>
2022-06-17 18:50:00 +0200

    Add files via upload

---
+++ LIB/BIT_MATH.h
@@ -0,0 +1,20 @@
+/*
+ * BIT_MATH.h
+ *
+ * Created: 9/15/2021 9:11:18 PM
+ *  Author: The Warlock & Omar
+ */ 
+
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define  SET_BIT(VAR,BIT_NUM)	VAR |=(1<<(BIT_NUM))
+#define  CLR_BIT(VAR,BIT_NUM)	VAR &= ~(1<<(BIT_NUM))
+#define	 TOG_BIT(VAR,BIT_NUM)	VAR ^= (1<<(BIT_NUM))
+#define	 GET_BIT(VAR,BIT_NUM)	((VAR>>(BIT_NUM))&1)
+
+#define CLR_REG(VAR)			VAR = 0
+#define SET_REG(VAR)			VAR = 255
+
+#endif /* BIT_MATH_H_ */
