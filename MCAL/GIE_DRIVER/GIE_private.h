40e31d261a47b8e52226b243e49514a1a3f55c68
Omar Mustafa <77558263+omarmustafa130@users.noreply.github.com>
2022-06-17 18:36:44 +0200

    Add files via upload

---
+++ MCAL/GIE_DRIVER/GIE_private.h
@@ -0,0 +1,16 @@
+/*
+ * GIE_private.h
+ *
+ * Created: 9/19/2021 3:14:51 PM
+ *  Author: AOM
+ */ 
+
+
+#ifndef GIE_PRIVATE_H_
+#define GIE_PRIVATE_H_
+
+#define SREG		*((volatile  u8*)(0x5F))
+
+
+
+#endif /* GIE_PRIVATE_H_ */
